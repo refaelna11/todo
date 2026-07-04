027f7fcfbd92a5df3e9b827e32e17d4f3e1f845c
Refael Shahar <refaelna11@gmail.com>
2026-07-04 20:56:00 +0300

    routes for Todo List

--- todo/routes/web.php
+++ todo/routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [TaskController::class, 'index'])->name('tasks.index');
+Route::post('/', [TaskController::class, 'store'])->name('tasks.store');
+Route::get('/{id}/edit', [TaskController::class, 'edit'])->name('tasks.edit');
+Route::put('/{id}', [TaskController::class, 'update'])->name('tasks.update');
+Route::delete('/{id}', [TaskController::class, 'destroy'])->name('tasks.destroy');
+Route::post('/{id}/toggle', [TaskController::class, 'toggle'])->name('tasks.toggle');
